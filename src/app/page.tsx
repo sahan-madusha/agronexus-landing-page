b67a82899f179de264514b1a75d2c00155380f79
sahan madusha <sahanmadusha001@gmail.com>
2025-12-30 20:42:45 +0530

    pipeline test 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import Footer from '../components/Footer'
 export default function Home() {
   return (
     <main className="min-h-screen">
+      test
       <HeroSection />
       <KeyFeatures />
       <HowItWorks />
